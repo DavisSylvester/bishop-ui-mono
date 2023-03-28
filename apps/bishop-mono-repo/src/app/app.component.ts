719c9af53310db1e615ae3755079ec75a65e4501
Davis Sylvester <davis@sylvesterllc.com>
2023-03-27 23:33:55 -0500

    Initial commit

---
+++ apps/bishop-mono-repo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bishop-mono-repo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'bishop-mono-repo';
+}
