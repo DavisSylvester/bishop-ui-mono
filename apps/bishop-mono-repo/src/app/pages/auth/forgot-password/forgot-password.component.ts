29f7e7fff4570bf25b6c2799330aac6417d76dc2
Davis Sylvester <davis@sylvesterllc.com>
2023-04-23 20:00:27 -0500

    Adding forgot password and OTP

--- apps/bishop-mono-repo/src/app/pages/auth/forgot-password/forgot-password.component.ts
+++ apps/bishop-mono-repo/src/app/pages/auth/forgot-password/forgot-password.component.ts
@@ -9,17 +9,65 @@ import { Router } from '@angular/router';
 })
 export class ForgotPasswordComponent {
 
+  showForgotPasswordComponent = true;
+  showOtpComponent = false;
+  otpEmailAddress =  '';
+
+
   private get email() {
+    return this.forgotPasswordForm.get("email")?.value;
+  }
+
+  private get otpCode() {
     return this.forgotPasswordForm.get("email")?.value;
   }
 
   public forgotPasswordForm: FormGroup = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    
+  });
+
+  public otpCodeForm: FormGroup = this.fb.group({
+    codeOne: ['', []],
+    codeTwo: ['', []],
+    codeThree: ['', []],
+    codeFour: ['', []],
+    codeFive: ['', []],
+    codeSix: ['', []],
   });
 
   constructor(private fb: FormBuilder, private router: Router) {}
-  
+
   public navigateTo() {}
-  public checkOTP() {}
+  public checkOTP() {
+    this.toggleComponents();
+  }
+  public resendOTP() {}
+  public cancelOTP() {
+    this.toggleComponents();
+  }
+  public onKeyUpChange(event: KeyboardEvent) {
+    console.log(event);
+  //   // let event = $event as HTMLInputElement
+  //   let element: HTMLElement;
+
+  //  if (event.code !== 'Backspace') {
+  //   // element = event.
+  //  }
+
+
+  //   if (event.code === 'Backspace')
+  //       element = event.target.;
+
+  //   if(!element!) {
+  //       return;
+  //   }
+  //   else{
+  //       element.focus();
+  //   }
+  }
+
+  private toggleComponents() {
+    this.showForgotPasswordComponent = !this.showForgotPasswordComponent;
+    this.showOtpComponent = !this.showOtpComponent;
+  }
 }
